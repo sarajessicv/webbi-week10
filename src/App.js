92f8e814ae46c42e86a102b4ee9015376f639b65
sarajessicv <sara.j.heikkinen@gmail.com>
2022-07-16 16:54:08 +0300

    4done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import MyContainer from './components/MyContainer.js'
 import About from './components/About.js'
 import Header from './components/Header.js'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { useTranslation } from 'react-i18next';
 
 function App() {
+
+  const { t, i18n } = useTranslation();
+    const changeLanguage = (lang) => {
+        i18n.changeLanguage(lang)
+    }
   return (
     <Router>
       <div className="App">
         <Header></Header>
         <Routes>
-          <Route path="/" element={<> <MyContainer /> </> } />
+          <Route path="/" element={<> <p>{t("This is the front page")}</p> </> } />
           <Route path="/about" element={<About />} />
         </Routes>
         
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,48 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import { Link as RouterLink, MemoryRouter } from 'react-router-dom';
-import Button from '@mui/material/Button';
-
+import Button from '@mui/material/Button'
+import { useTranslation } from 'react-i18next';
+import { Box } from '@mui/system';
+import React, { Suspense } from 'react'
 
 
 function Header() {
+    const { t, i18n } = useTranslation();
+    const changeLanguage = (lang) => {
+        i18n.changeLanguage(lang)
+    }
+
+
     return (
         <div>
             <AppBar position="static">
                 <Toolbar variant="dense">
-                    <Button sx={{color:'white'}} component={RouterLink} to="/">
-                        HOME
+                    <Button sx={{ color: 'white' }} component={RouterLink} to="/">
+                        {t("HOME")}
                     </Button>
-                    <Button sx={{color:'white'}} component={RouterLink} to="/about">
-                        ABOUT
+                    <Button sx={{ color: 'white'}} component={RouterLink} to="/about">
+                        {t("ABOUT")}
                     </Button>
+                    <Box marginLeft={"100px"}>
+                        <Button id="fi" onClick={() => changeLanguage("fi")} sx={{ color: 'white' }}>FI</Button>
+                        <Button id="en" onClick={() => changeLanguage("en")} sx={{ color: 'white' }}>EN</Button>
+                    </Box>
                 </Toolbar>
             </AppBar>
-        </div>
+        </div >
     )
 }
 
-export default Header
+//export default Header
+
+
+export default function App() {
+    return (
+        <Suspense fallback="loading">
+            <Header />
+        </Suspense>
+    )
+
+}
+
