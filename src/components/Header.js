de41b61362ce4bd1b9c249fea91b22541d5eeb58
sarajessicv <sara.j.heikkinen@gmail.com>
2022-07-16 15:56:00 +0300

    3done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import { Link as RouterLink, MemoryRouter } from 'react-router-dom';
+import Button from '@mui/material/Button';
+
+
+
 function Header() {
-  return (
-    <div>
-      <h2>Header</h2>
-    </div>
-  )
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Button sx={{color:'white'}} component={RouterLink} to="/">
+                        HOME
+                    </Button>
+                    <Button sx={{color:'white'}} component={RouterLink} to="/about">
+                        ABOUT
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
 }
 
 export default Header
