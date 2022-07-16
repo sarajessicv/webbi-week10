f7cf1487cb24039bae4355614fe9bc2472a1fbb9
sarajessicv <sara.j.heikkinen@gmail.com>
2022-07-16 15:02:34 +0300

    1done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import MyContainer from './components/MyContainer.js'
+import About from './components/About.js'
+import Header from './components/Header.js'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello World!</h1>
-      <MyContainer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<> <MyContainer /> </> } />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        
+      </div>
+    </Router>
   );
 }
 
