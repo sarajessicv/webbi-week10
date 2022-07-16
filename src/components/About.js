49993862f83817c7f690ec47768694ce3fd69848
sarajessicv <sara.j.heikkinen@gmail.com>
2022-07-16 15:28:26 +0300

    1done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,23 @@
+import React, {useState, useEffect} from 'react';
+
 
 function About() {
+    const [dataList, setDataList] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(data => setDataList(data))
+      },[])
+
+    const listItems = dataList.map((element) => (
+        <li key={element.id}>{element.title}</li>
+    ))
+
   return (
     <div>
       <h2>About</h2>
+      <ul>{listItems}</ul>
     </div>
   )
 }
